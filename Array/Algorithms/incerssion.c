6a1dfc1237061e3ebf183e5f2040679ef81703eb
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-08 14:37:28 +0530

    Update incerssion.c

--- Array/Algorithms/incerssion.c
+++ Array/Algorithms/incerssion.c
@@ -1,29 +1,32 @@
-// insertion - adding element at the last
+// insertion array program
+
 #include <stdio.h>
 int main() {
     int n;
-    printf("Enter the number of elements: ");
+    printf("Enter the number of elements: "); 
     scanf("%d", &n);
 
-    int arr[n+1]; // allocate extra space for insertion
+    int arr[n+1]; // space for one extra element
 
-    // taking input
-    printf("Enter %d elements:\n", n);
+    // taking element input
+    printf("Enter %d elements:\n", n); 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    // displaying the array
+    
+    // displaying the created array
     printf("Created array:\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 
+    // Ask for position to insert
     int position, element;
-    printf("Enter the position (0 to %d) where you want to insert new element: ", n);
+    printf("Enter the position (0 to %d) where you want to insert: ", n);
     scanf("%d", &position);
 
+    // asking for the element to add 
     printf("Enter the element you want to insert: ");
     scanf("%d", &element);
 
@@ -31,14 +34,18 @@ int main() {
         printf("Invalid position!\n");
     } else {
         // shift elements to the right
-        for (int i = n; i > position; i--) {
+        int i = n;
+        while (i > position) {
             arr[i] = arr[i - 1];
+            i--;
         }
-        arr[position] = element; // insert element
+        arr[position] = element; //  insert the element
 
         printf("Array after insertion:\n");
-        for (int i = 0; i <= n; i++) {
-            printf("%d ", arr[i]);
+        int j = 0;
+        while (j <= n) { // ✅ print n+1 elements
+            printf("%d ", arr[j]);
+            j++;
         }
         printf("\n");
     }
